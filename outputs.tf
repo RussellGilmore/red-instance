2aa0cadca9ddac668beefd2e1261eae5a385d1b5
Russell Gilmore <RussellGilmore@live.com>
2026-06-24 13:26:56 -0400

    Removed key related outputs

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,29 @@
-output "key_name" {
-  value       = var.create_ec2_key_pair ? aws_key_pair.red_key[0].key_name : "A Key pair was not created for this instance"
-  description = "The name of the key pair"
-}
-
-output "key_fingerprint" {
-  value       = var.create_ec2_key_pair ? aws_key_pair.red_key[0].fingerprint : "A Key pair was not created for this instance"
-  description = "The fingerprint of the key pair"
-}
-
-output "private_key_path" {
-  value       = var.create_ec2_key_pair ? local_file.red_private_key_file[0].filename : "A Key pair was not created for this instance"
-  description = "The path to the private key file"
+output "instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = aws_instance.red-instance.id
 }
 
 output "vpc_id" {
+  description = "The ID of the created VPC, or a note when an existing VPC was supplied."
   value       = var.create_vpc ? aws_vpc.main[0].id : "VPC was inherited from another module or resource"
-  description = "The ID of the created VPC"
 }
 
 output "subnet_id" {
+  description = "The ID of the created subnet, or a note when an existing subnet was supplied."
   value       = var.create_vpc ? aws_subnet.public[0].id : "Subnet was inherited from another module or resource"
-  description = "The ID of the created subnet"
 }
 
 output "public_ip" {
+  description = "The public IP address of the instance (EIP when allocated)."
   value       = var.allocate_eip ? aws_eip.red_instance_eip[0].public_ip : "Public IP not allocated"
-  description = "The public IP address of the instance"
 }
 
 output "public_dns" {
+  description = "The public DNS name of the instance."
   value       = var.enable_public_dns ? aws_route53_record.red_instance_dns[0].fqdn : "Public DNS not allocated"
-  description = "The public DNS name of the instance"
+}
+
+output "security_group_id" {
+  description = "The ID of the instance security group."
+  value       = aws_security_group.red_sg.id
 }
